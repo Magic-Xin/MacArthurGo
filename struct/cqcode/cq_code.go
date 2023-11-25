98cf117700f49f8cfa535e5cadf3f2c98c9777f4
magicxin <x.yu2000@163.com>
2023-11-25 20:30:10 +0800

    fix CQcode escape

--- struct/cqcode/cq_code.go
+++ struct/cqcode/cq_code.go
@@ -14,6 +14,10 @@ type CQCode struct {
 func (cq *CQCode) toString() string {
 	res := fmt.Sprintf("CQ:%s", cq.Type)
 	for k, v := range cq.Data {
+		switch v.(type) {
+		case string:
+			v = EscapeInsideCQ(v.(string))
+		}
 		res += fmt.Sprintf(",%s=%v", k, v)
 	}
 	return fmt.Sprintf("[%s]", res)
@@ -49,7 +53,7 @@ func Escape(str string, insideCQ bool) string {
 	}
 
 	s = strings.ReplaceAll(s, ",", "&#44;")
-	s = regexp.MustCompile(`([\uD800-\uDBFF][\uDC00-\uDFFF])`).ReplaceAllString(s, " ")
+	//s = regexp.MustCompile(`([\uD800-\uDBFF][\uDC00-\uDFFF])`).ReplaceAllString(s, " ")
 
 	return s
 }
