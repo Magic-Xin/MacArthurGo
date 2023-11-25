a4ad3c70fbddff662ed3c2a2e5db66a56c2e4b9c
magicxin <x.yu2000@163.com>
2023-11-25 20:29:41 +0800

    add echo action

--- struct/action.go
+++ struct/action.go
@@ -1,6 +1,16 @@
 package _struct
 
 type Action struct {
-	Action string      `json:"action"`
-	Params interface{} `json:"params"`
+	Action string `json:"action"`
+	Params any    `json:"params"`
+}
+
+type EchoAction struct {
+	Action string `json:"action"`
+	Params any    `json:"params"`
+	Echo   string `json:"echo"`
+}
+
+type GetMsg struct {
+	Id int `json:"message_id"`
 }
