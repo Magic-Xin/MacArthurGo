b5f35069ec9b34b5b62057d73d3743d7b219960d
magicxin <x.yu2000@163.com>
2023-11-28 04:12:06 +0800

    add config initialization

---
+++ config/config.go
@@ -0,0 +1,13 @@
+package config
+
+import (
+	"github.com/gookit/config/v2"
+	"log"
+)
+
+func init() {
+	err := config.LoadFiles("config.json")
+	if err != nil {
+		log.Fatalf("Can not find config error: %v", err)
+	}
+}
