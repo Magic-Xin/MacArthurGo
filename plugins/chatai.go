ad83278ae2a46a8b4628634d3db6e6de5f548a78
magicxin <x.yu2000@163.com>
2024-01-28 16:31:19 +0800

    chore: add random ip

--- plugins/chatai.go
+++ plugins/chatai.go
@@ -20,11 +20,14 @@ import (
 	"image/jpeg"
 	"io"
 	"log"
+	"math/rand"
+	"net"
 	"net/http"
 	"regexp"
 	"strconv"
 	"strings"
 	"sync"
+	"time"
 )
 
 type ChatGPT struct {
@@ -52,6 +55,7 @@ type NewBing struct {
 	Enabled bool
 	Args    []string
 	model   string
+	IpRange [][]string
 }
 
 type ChatAI struct {
@@ -86,6 +90,38 @@ func init() {
 		Enabled: base.Config.Plugins.ChatAI.NewBing.Enable,
 		Args:    base.Config.Plugins.ChatAI.NewBing.Args,
 		model:   base.Config.Plugins.ChatAI.NewBing.Model,
+		IpRange: [][]string{
+			{"4.150.64.0", "4.150.127.255"},      // Azure Cloud EastUS2 16382
+			{"4.152.0.0", "4.153.255.255"},       // Azure Cloud EastUS2 131070
+			{"13.68.0.0", "13.68.127.255"},       // Azure Cloud EastUS2 32766
+			{"13.104.216.0", "13.104.216.255"},   // Azure EastUS2 256
+			{"20.1.128.0", "20.1.255.255"},       // Azure Cloud EastUS2 32766
+			{"20.7.0.0", "20.7.255.255"},         // Azure Cloud EastUS2 65534
+			{"20.22.0.0", "20.22.255.255"},       // Azure Cloud EastUS2 65534
+			{"40.84.0.0", "40.84.127.255"},       // Azure Cloud EastUS2 32766
+			{"40.123.0.0", "40.123.127.255"},     // Azure Cloud EastUS2 32766
+			{"4.214.0.0", "4.215.255.255"},       // Azure Cloud JapanEast 131070
+			{"4.241.0.0", "4.241.255.255"},       // Azure Cloud JapanEast 65534
+			{"40.115.128.0", "40.115.255.255"},   // Azure Cloud JapanEast 32766
+			{"52.140.192.0", "52.140.255.255"},   // Azure Cloud JapanEast 16382
+			{"104.41.160.0", "104.41.191.255"},   // Azure Cloud JapanEast 8190
+			{"138.91.0.0", "138.91.15.255"},      // Azure Cloud JapanEast 4094
+			{"151.206.65.0", "151.206.79.255"},   // Azure Cloud JapanEast 256
+			{"191.237.240.0", "191.237.241.255"}, // Azure Cloud JapanEast 512
+			{"4.208.0.0", "4.209.255.255"},       // Azure Cloud NorthEurope 131070
+			{"52.169.0.0", "52.169.255.255"},     // Azure Cloud NorthEurope 65534
+			{"68.219.0.0", "68.219.127.255"},     // Azure Cloud NorthEurope 32766
+			{"65.52.64.0", "65.52.79.255"},       // Azure Cloud NorthEurope 4094
+			{"98.71.0.0", "98.71.127.255"},       // Azure Cloud NorthEurope 32766
+			{"74.234.0.0", "74.234.127.255"},     // Azure Cloud NorthEurope 32766
+			{"4.151.0.0", "4.151.255.255"},       // Azure Cloud SouthCentralUS 65534
+			{"13.84.0.0", "13.85.255.255"},       // Azure Cloud SouthCentralUS 131070
+			{"4.255.128.0", "4.255.255.255"},     // Azure Cloud WestCentralUS 32766
+			{"13.78.128.0", "13.78.255.255"},     // Azure Cloud WestCentralUS 32766
+			{"4.175.0.0", "4.175.255.255"},       // Azure Cloud WestEurope 65534
+			{"13.80.0.0", "13.81.255.255"},       // Azure Cloud WestEurope 131070
+			{"20.73.0.0", "20.73.255.255"},       // Azure Cloud WestEurope 65534
+		},
 	}
 
 	var args []string
@@ -490,7 +526,40 @@ func (g *Gemini) ImageProcessing(url string) (*[]byte, string, error) {
 func (n *NewBing) RequireAnswer(str string) *string {
 	const cookie = "1f1e33"
 
+	RandomIP := func() string {
+		seed := time.Now().UnixNano()
+		rng := rand.New(rand.NewSource(seed))
+
+		randomIndex := rng.Intn(len(n.IpRange))
+
+		startIP := n.IpRange[randomIndex][0]
+		endIP := n.IpRange[randomIndex][1]
+
+		ipToUint32 := func(ip net.IP) uint32 {
+			ip = ip.To4()
+			var result uint32
+			result += uint32(ip[0]) << 24
+			result += uint32(ip[1]) << 16
+			result += uint32(ip[2]) << 8
+			result += uint32(ip[3])
+			return result
+		}
+		uint32ToIP := func(intIP uint32) string {
+			ip := fmt.Sprintf("%d.%d.%d.%d", byte(intIP>>24), byte(intIP>>16), byte(intIP>>8), byte(intIP))
+			return ip
+		}
+
+		startIPInt := ipToUint32(net.ParseIP(startIP))
+		endIPInt := ipToUint32(net.ParseIP(endIP))
+
+		randomIPInt := rng.Uint32()%(endIPInt-startIPInt+1) + startIPInt
+		randomIP := uint32ToIP(randomIPInt)
+
+		return randomIP
+	}
+
 	c := binglib.NewChat(cookie)
+	c.SetXFF(RandomIP())
 	err := c.NewConversation()
 	if err != nil {
 		log.Printf("NewBing new conversation error: %v", err)
