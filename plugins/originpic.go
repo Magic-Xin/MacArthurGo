48a46ed56f778d57d73921284b50eaf73b2a6816
magicxin <x.yu2000@163.com>
2024-02-10 23:06:11 +0800

    `chore`: update reply response

--- plugins/originpic.go
+++ plugins/originpic.go
@@ -3,7 +3,14 @@ package plugins
 import (
 	"MacArthurGo/base"
 	"MacArthurGo/plugins/essentials"
+	"MacArthurGo/structs"
 	"MacArthurGo/structs/cqcode"
+	"errors"
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
 )
 
 type OriginPic struct {
@@ -40,13 +47,104 @@ func (o OriginPic) ReceiveMessage(ctx *map[string]any, send *chan []byte) {
 	var reply []cqcode.ArrayMessage
 	for _, m := range *message {
 		if m.Type == "image" {
-			reply = append(reply, *cqcode.Image(m.Data["url"].(string)))
+			fileUrl, _ := essentials.GetUniversalImgURL(m.Data["url"].(string))
+			reply = append(reply, *cqcode.Image(fileUrl))
+		}
+		if m.Type == "reply" {
+			mid := int64(m.Data["id"].(float64))
+			*send <- *essentials.SendAction("get_msg", structs.GetMsg{Id: mid}, "originPic")
+			return
 		}
 	}
 
 	if len(reply) > 0 {
-		*send <- *essentials.SendMsg(ctx, "", &reply, false, true)
+		*send <- *essentials.SendMsg(ctx, "", &reply, false, false)
 	}
 }
 
-func (o OriginPic) ReceiveEcho(*map[string]any, *chan []byte) {}
+func (o OriginPic) ReceiveEcho(ctx *map[string]any, send *chan []byte) {
+	if !o.Enabled {
+		return
+	}
+
+	if (*ctx)["status"].(string) != "ok" {
+		return
+	}
+
+	echo := (*ctx)["echo"].(string)
+	split := strings.Split(echo, "|")
+
+	if split[0] == "originPic" {
+		contexts := (*ctx)["data"].(map[string]any)
+		message := essentials.DecodeArrayMessage(&contexts)
+		if message == nil {
+			return
+		}
+
+		for _, m := range *message {
+			if m.Type == "image" {
+				originUrl := *essentials.GetOriginUrl(m.Data["url"].(string))
+				imgType, err := o.getFileType(originUrl)
+				if err != nil {
+					log.Printf("Image type error: %v", err)
+					continue
+				}
+				if imgType == "gif" {
+					params := struct {
+						Url string `json:"url"`
+					}{Url: originUrl}
+					echo := "originPicFile|" + contexts["message_type"].(string)
+					if contexts["message_type"].(string) == "group" {
+						echo += "|" + strconv.FormatInt(int64(contexts["group_id"].(float64)), 10)
+					} else {
+						echo += "|" + strconv.FormatInt(int64(contexts["user_id"].(float64)), 10)
+					}
+					echo += "|" + m.Data["file"].(string)
+
+					*send <- *essentials.SendAction("download_file", params, echo)
+				} else {
+					*send <- *essentials.SendMsg(&contexts, "", &[]cqcode.ArrayMessage{*cqcode.Image(originUrl)}, false, false)
+				}
+			}
+		}
+	} else if split[0] == "originPicFile" {
+		id, _ := strconv.ParseFloat(split[2], 64)
+		contexts := &map[string]any{
+			"message_type": split[1],
+			"sender": map[string]any{
+				"user_id": id,
+			},
+			"group_id": id,
+		}
+		file := (*ctx)["data"].(map[string]any)["file"].(string)
+		*send <- *essentials.SendFile(contexts, file, split[3]+".gif")
+	}
+}
+
+func (o OriginPic) getFileType(url string) (string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer func(Body io.ReadCloser) {
+		err = Body.Close()
+		if err != nil {
+			log.Printf("Image fetch close error: %v", err)
+		}
+	}(resp.Body)
+
+	imgData, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	switch imgType := http.DetectContentType(imgData); imgType {
+	case "image/jpeg":
+		return "jpeg", nil
+	case "image/png":
+		return "png", nil
+	case "image/gif":
+		return "gif", nil
+	default:
+		return "", errors.New("unsupported image type")
+	}
+}
